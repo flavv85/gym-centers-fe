acc9511982e3af273648ae97d96e9d20c671cdc1
FlaviusGorgan <gorganflavius@gmail.com>
2021-07-14 23:42:35 +0300

    admin-dashboard

---
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Customer, Gym, User, Workout } from '../GymCentersModal';
+import { CustomerService } from '../service/customer.service';
+import { GymService } from '../service/gym.service';
+import { UserStorageService } from '../service/user-storage.service';
+
+@Component({
+  selector: 'app-admin-dashboard',
+  templateUrl: './admin-dashboard.component.html',
+  styleUrls: ['./admin-dashboard.component.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class AdminDashboardComponent implements OnInit {
+  gym: Gym = {} as Gym;
+  user: User = {} as User;
+  customer: Customer = {} as Customer;
+  workout: Workout = {} as Workout;
+  username: string;
+  gender: any = {};
+  showProgressBar = false;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private userStorageService: UserStorageService,
+    private gymService: GymService,
+    private customerService: CustomerService
+  ) {}
+
+  ngOnInit(): void {
+    this.username = this.route.snapshot.params.username;
+    this.userStorageService
+      .findUserByUsername(this.username)
+      .subscribe((user) => {
+        this.user = user;
+      });
+  }
+  setGender() {
+    this.customer.sex = this.gender;
+  }
+  addCustomer() {
+    this.showProgressBar = true;
+    this.customerService.addCustomer(this.customer, 1).subscribe((customer) => {
+      this.customer = customer;
+      window.location.reload();
+    });
+  }
+  addGym() {
+    this.showProgressBar = true;
+    this.gymService.addGym(this.gym).subscribe((gym) => {
+      this.gym = gym;
+      window.location.reload();
+    });
+  }
+
+  home() {
+    window.location.replace(`/home/${this.username}`);
+  }
+
+  logout(id: number) {
+    this.userStorageService.signOut();
+    this.router.navigateByUrl('/login');
+    this.userStorageService.deleteUser(id).subscribe();
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, AdminDashboardComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
